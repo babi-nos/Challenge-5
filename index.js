10565c70e029cb91d22e4faa1c4e2d6fed386abf
Marvin <124067182+babi-nos@users.noreply.github.com>
2023-04-11 19:28:37 +0200

    challenge 5 carrousel finish 90%

--- index.js
+++ index.js
@@ -15,13 +15,20 @@ const nbSlideMobile = allImg.length;
 const rightMobile = document.querySelector(".right-mobile");
 const leftMobile = document.querySelector(".left-mobile");
 
+// btn menu hamburger 
+const burger = document.querySelector(".line-menu");
+const navItem = document.querySelector(".nav-links");
+const btnClose = document.querySelector(".btn-close");
+const containerAfter = document.getElementById('container');
 
+
+// Btn left left image
 
 function slideSuivante(){
     allImg[count].classList.remove('active');
     allImgMobile[count].classList.remove('active');
     allDescription[count].classList.remove('active');
-
+    
     if ((count < nbSlide - 1), (count <  numberDescritpion - 1 ), (count <  nbSlideMobile - 1 )){
         count++;
     } else {
@@ -36,12 +43,15 @@ function slideSuivante(){
 right.addEventListener('click', slideSuivante);
 rightMobile.addEventListener('click', slideSuivante);
 
+
+// btn left right mobile
+
 function slidePrecedent(){
     allImg[count].classList.remove('active');
     allImgMobile[count].classList.remove('active');
     allDescription[count].classList.remove('active');
-
-
+    
+    
     if (count > 0 ){
         count--;
     } else {
@@ -51,9 +61,28 @@ function slidePrecedent(){
     allImg[count].classList.add('active')
     allImgMobile[count].classList.add('active')
     allDescription[count].classList.add('active')
-
+    
 } 
 
 left.addEventListener('click', slidePrecedent);
 leftMobile.addEventListener('click', slidePrecedent);
 
+
+
+// btn menu hamburger 
+
+function menuHamburger(){
+    navItem.style.left = 0
+
+    containerAfter.style.opacity = 0.5
+
+    btnClose.addEventListener('click', (e) => {
+        if(e){
+            navItem.style.left = '101%';
+            containerAfter.style.opacity = 1
+        }
+    })
+
+}
+
+burger.addEventListener('click', menuHamburger);
